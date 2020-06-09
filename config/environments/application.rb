44a9509c49c15efdf508191d97edb9a526a431c4
minahood <flost.ak7@gmail.com>
2020-06-09 06:05:01 +0000

    Initialize repository

---
+++ config/environments/application.rb
@@ -0,0 +1,7 @@
+config.generators do |g|
+  g.test_framework :rspec, 
+        view_specs: false, 
+        helper_specs: false, 
+        controller_specs: false, 
+        routing_specs: false
+end
