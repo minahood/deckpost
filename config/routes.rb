ec2c39b9d7862b5cdc545baf93520c0eba4b0346
minahood <flost.ak7@gmail.com>
2020-06-17 07:54:23 +0000

    Add Sessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup',to: 'users#new'
   get '/help',to:'static_pages#help'
   get '/about',to:'static_pages#about'
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
   get '/news',to:'static_pages#news'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   resources :users
   
