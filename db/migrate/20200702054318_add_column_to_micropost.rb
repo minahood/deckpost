641604626e7f117be3d0eead9032bc71222d18a4
minahood <flost.ak7@gmail.com>
2020-07-02 06:03:47 +0000

    Fix db migration a little

--- db/migrate/20200702054318_add_column_to_micropost.rb
+++ db/migrate/20200702054318_add_column_to_micropost.rb
@@ -1,6 +1,7 @@
 class AddColumnToMicropost < ActiveRecord::Migration[6.0]
   def change
     add_column :microposts, :title, :string
+    add_index :microposts, :title
   end
-  add_index :microposts, :title
+  
 end
